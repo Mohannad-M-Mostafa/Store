a5ce2dd97bb672e9512f3f2a189f3873919b7281
Adham Khatean <aekhatean@gmail.com>
2024-06-08 00:06:59 +0300

    fix router issues

--- src/index.js
+++ src/index.js
@@ -2,31 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Login from "./components/Login";
-import Document from "./components/Dashboard/Document";
-
-const Routes = createBrowserRouter([
-  {
-    path: "/",
-    element: <Login />,
-  },
-  {
-    path: "/home",
-    element: <App />,
-  },
-  {
-    path: "/document",
-    element: <Document />,
-  },
-]);
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <RouterProvider router={Routes}>
-    <App />
-  </RouterProvider>
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
-
-reportWebVitals();
